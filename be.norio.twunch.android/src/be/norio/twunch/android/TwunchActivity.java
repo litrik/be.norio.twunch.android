f47a702c5bb4dbba37ddd1f94daddd4de3b47881
Litrik De Roy <litrik@norio.be>
2010-03-22 18:44:36 +0000

    Added menu item for to share a Twunch. Added menu item to show map of a Twunch. Added menu item to register for a Twunch. http://code.google.com/p/twunch/issues/detail?id=12

--- be.norio.twunch.android/src/be/norio/twunch/android/TwunchActivity.java
+++ be.norio.twunch.android/src/be/norio/twunch/android/TwunchActivity.java
@@ -29,6 +29,8 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.text.format.DateUtils;
 import android.text.util.Linkify;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -38,6 +40,10 @@ import be.norio.twunch.android.core.Twunch;
 public class TwunchActivity extends Activity {
 
 	public static String PARAMETER_INDEX = "index";
+
+	private final static int MENU_MAP = 0;
+	private final static int MENU_REGISTER = 1;
+	private final static int MENU_SHARE = 2;
 
 	private Twunch twunch;
 
@@ -56,19 +62,13 @@ public class TwunchActivity extends Activity {
 		((Button) findViewById(R.id.ButtonMap)).setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				String uri = twunch.getMap();
-				final Intent myIntent = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse(uri));
-				startActivity(myIntent);
+				doMap();
 			}
 		});
 		((Button) findViewById(R.id.ButtonJoin)).setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				final Intent intent = new Intent(Intent.ACTION_SEND);
-				intent.setType("text/plain");
-				intent.putExtra(Intent.EXTRA_TEXT, "@twunch "
-						+ String.format(getString(R.string.tweet), twunch.getTitle(), twunch.getLink()));
-				startActivity(Intent.createChooser(intent, "Send tweet"));
+				doRegister();
 			}
 		});
 	}
@@ -96,4 +96,72 @@ public class TwunchActivity extends Activity {
 				.getString(R.string.participants), twunch.getNumberOfParticipants()));
 		((TextView) view.findViewById(R.id.twunchExtra)).setText(extra);
 	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.app.Activity#onCreateOptionsMenu(android.view.Menu)
+	 */
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		menu.add(0, MENU_MAP, 0, R.string.button_map).setIcon(R.drawable.ic_menu_mapmode);
+		menu.add(0, MENU_REGISTER, 0, R.string.button_register).setIcon(R.drawable.ic_menu_add);
+		menu.add(0, MENU_SHARE, 0, R.string.menu_share).setIcon(R.drawable.ic_menu_share);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.app.Activity#onOptionsItemSelected(android.view.MenuItem)
+	 */
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case MENU_MAP:
+			doMap();
+			return true;
+		case MENU_REGISTER:
+			doRegister();
+			return true;
+		case MENU_SHARE:
+			doShare();
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Show the location of this Twunch on a map.
+	 */
+	private void doMap() {
+		String uri = twunch.getMap();
+		final Intent myIntent = new Intent(android.content.Intent.ACTION_VIEW, Uri.parse(uri));
+		startActivity(myIntent);
+	}
+
+	/**
+	 * Register for this Twunch.
+	 */
+	private void doRegister() {
+		final Intent intent = new Intent(Intent.ACTION_SEND);
+		intent.setType("text/plain");
+		intent.putExtra(Intent.EXTRA_TEXT, "@twunch "
+				+ String.format(getString(R.string.register_text), twunch.getTitle(), twunch.getLink()));
+		startActivity(Intent.createChooser(intent, getString(R.string.register_title)));
+	}
+
+	/**
+	 * Share information about this Twunch.
+	 */
+	private void doShare() {
+		final Intent intent = new Intent(Intent.ACTION_SEND);
+		intent.setType("text/plain");
+		intent.putExtra(Intent.EXTRA_TEXT, String.format(getString(R.string.share_text), twunch.getTitle(), DateUtils
+				.formatDateTime(this, twunch.getDate().getTime(), DateUtils.FORMAT_SHOW_WEEKDAY | DateUtils.FORMAT_SHOW_DATE),
+				DateUtils.formatDateTime(this, twunch.getDate().getTime(), DateUtils.FORMAT_SHOW_TIME), twunch.getLink()));
+		intent.putExtra(Intent.EXTRA_TEXT, getString(R.string.share_subject));
+		startActivity(Intent.createChooser(intent, getString(R.string.share_title)));
+	}
+
 }
