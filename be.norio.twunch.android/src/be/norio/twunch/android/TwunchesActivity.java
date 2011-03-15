187a9be2f7e5efa3ef487fef5648386d6bc72517
Litrik De Roy <litrik@norio.be>
2011-03-15 16:52:52 +0000

    Fixed (kinda) calculation of number of days between two dates.

--- be.norio.twunch.android/src/be/norio/twunch/android/TwunchesActivity.java
+++ be.norio.twunch.android/src/be/norio/twunch/android/TwunchesActivity.java
@@ -161,7 +161,9 @@ public class TwunchesActivity extends GDActivity {
 			((TextView) view.findViewById(R.id.twunchDate)).setTypeface(null, cursor.getInt(COLUMN_DISPLAY_NEW) == 1 ? Typeface.BOLD
 					: Typeface.NORMAL);
 			// Days
-			int days = (int) ((cursor.getLong(COLUMN_DISPLAY_DATE) - new Date().getTime()) / 1000 / 60 / 60 / 24);
+			// TODO: This is not the most smart/correct way to calculate the number of
+			// days between 2 dates
+			int days = (int) Math.ceil(((cursor.getLong(COLUMN_DISPLAY_DATE) - new Date().getTime()) / 1000 / 60 / 60 / 24) + 0.5);
 			((TextView) view.findViewById(R.id.twunchDays)).setText(days == 0 ? getString(R.string.today) : String.format(
 					getResources().getQuantityString(R.plurals.days_to_twunch, days), days));
 		}
