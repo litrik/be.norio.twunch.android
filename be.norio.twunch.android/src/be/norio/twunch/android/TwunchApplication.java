f13cf4ec5f1866118781030da3671b7d70f15885
Litrik De Roy <litrik@norio.be>
2012-02-23 10:54:40 +0100

    Fixed copyrights.

--- be.norio.twunch.android/src/be/norio/twunch/android/TwunchApplication.java
+++ be.norio.twunch.android/src/be/norio/twunch/android/TwunchApplication.java
@@ -1,5 +1,5 @@
 /**
- *	Copyright 2010-2011 Norio bvba
+ *	Copyright 2010-2012 Norio bvba
  *
  *	This program is free software: you can redistribute it and/or modify
  *	it under the terms of the GNU General Public License as published by
